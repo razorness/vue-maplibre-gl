2ec8a3f772ae7132b82e497031ee51c1b6486a83
Volker Nauruhn <684302+razorness@users.noreply.github.com>
2021-08-24 19:50:29 +0200

    useMap returns reactive object

--- src/components/mapRegistry.ts
+++ src/components/mapRegistry.ts
@@ -1,17 +1,31 @@
 import { MglMap } from '@/components/index';
 import maplibregl from 'maplibre-gl';
+import { reactive } from 'vue';
 
-const instances  = new Map<symbol | string, InstanceType<typeof MglMap>>(),
+export interface MapInstance {
+	component?: InstanceType<typeof MglMap>;
+	map?: maplibregl.Map;
+}
+
+const instances  = new Map<symbol | string, MapInstance>(),
 	  defaultKey = Symbol('default');
 
-export function useMap(key: symbol | string = defaultKey): { component: InstanceType<typeof MglMap> | undefined, map: maplibregl.Map | undefined } {
-	const component = instances.get(key);
-	return { component, map: component?.map as maplibregl.Map };
+// useMap returns reactive version of MapInstance
+export function useMap(key: symbol | string = defaultKey): MapInstance {
+	let component = instances.get(key);
+	if (!component) {
+		component = {};
+		instances.set(key, reactive(component));
+	}
+	return component;
 }
 
 export function registerMap(instance: InstanceType<typeof MglMap>, key: symbol | string = defaultKey) {
-	if (instances.has(key)) {
+	const component = instances.get(key);
+	if (!component) {
+		instances.set(key, reactive({ component: instance, map: instance.map as maplibregl.Map }));
 		return;
 	}
-	instances.set(key, instance);
+	component.component = instance;
+	component.map       = instance.map as maplibregl.Map;
 }
