184f7d1c435afbf7870e5ff53b0c0b6f9ae38e0c
razorness <684302+razorness@users.noreply.github.com>
2025-02-15 15:15:13 +0100

    better types export

--- src/plugins/draw/index.ts
+++ src/plugins/draw/index.ts
@@ -3,5 +3,7 @@ import './draw.plugin.scss';
 export * from './plugin';
 export * from './polygon.mode.ts';
 export * from './circle.mode.ts';
-export * from './circleStatic.mode.ts'
+export * from './circleStatic.mode.ts';
 export * from './types';
+
+export { default as MglDrawControl } from './draw.control';
